c461e276c0a21f51d68e684a1559552b33fc1cb6
Marcos Brizeno <marcos.uece.comp@gmail.com>
2024-03-08 07:00:03 -0600

    Add initial front-end and api code

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	err := app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
